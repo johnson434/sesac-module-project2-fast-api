780a46bb7d48ca86d9e200678aa7c2fd73a33727
johnson434 <ghkddlsrb96@gmail.com>
2024-09-26 14:57:19 +0900

    FastAPI first init

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, APIRouter, HTTPException
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, log_level="debug")
