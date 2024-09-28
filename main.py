b470521727681e61463ba28b0b0ce68c2a4431e9
johnson434 <ghkddlsrb96@gmail.com>
2024-09-28 14:16:24 +0900

    계산기 로직 추가

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI, APIRouter, HTTPException
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
 origins = [
     "http://test.com",
+    "http://test.com:80",
     "http://test.com:3000",
     "http://localhost",
-    "http://localhost:3000",
+    "http://localhost:3000"
 ]
 
 app.add_middleware(
@@ -18,10 +20,17 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-@app.get("/api")
+@app.get("/calculator")
+async def get_multiplication_table(number: int = Query(..., description="구구단을 위한 숫자를 입력하세요")):
+    result = [f"{number} x {i} = {number * i}" for i in range(1, 10)]
+    
+    return {"result": result}
+
+
+@app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, log_level="debug")
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True, log_level="debug")
