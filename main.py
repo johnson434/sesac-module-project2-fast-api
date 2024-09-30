79c09b21de5729385eb82f1c6b35b48ac024fd54
johnson434 <ghkddlsrb96@gmail.com>
2024-09-30 16:29:14 +0900

    Dockerfile 주석 제거

--- main.py
+++ main.py
@@ -4,22 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://test.com",
-    "http://test.com:80",
-    "http://test.com:3000",
-    "http://localhost",
-    "http://localhost:3000"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
 @app.get("/api/calculator")
 async def get_multiplication_table(number: int = Query(..., description="구구단을 위한 숫자를 입력하세요")):
     result = [f"{number} x {i} = {number * i}" for i in range(1, 10)]
